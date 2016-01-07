eb8422ac53b9a59ec5d2c4f759425dc71ef3fcd5
Måns Olsson <mans2111@gmail.com>
2016-01-07 07:00:06 +0100

    Added keybindings for all keys

--- c/src/emulator.c
+++ c/src/emulator.c
@@ -18,28 +18,11 @@ void start_program(char *program_path)
 	load_program(&c, program_path);
 
 	create_window();
-	SDL_Event e;
 	long cd = 0;
 
 	while(game_running) {
 		long before = get_current_time_ms();
-		while( SDL_PollEvent( &e ) != 0 ) {
-			if( e.type == SDL_QUIT ) {
-				game_running = false;
-			} else if(e.type == SDL_KEYDOWN) {
-				if(e.key.keysym.sym >= SDLK_KP_1 && e.key.keysym.sym <= SDLK_KP_9) {
-					c.keys[e.key.keysym.sym - SDLK_KP_1 + 1] = true;
-				} else if(e.key.keysym.sym == SDLK_KP_A) {
-					c.keys[0] = true;
-				}
-			} else if(e.type == SDL_KEYUP) {
-				if(e.key.keysym.sym >= SDLK_KP_1 && e.key.keysym.sym <= SDLK_KP_9) {
-					c.keys[e.key.keysym.sym - SDLK_KP_1 + 1] = false;
-				} else if(e.key.keysym.sym == SDLK_KP_A) {
-					c.keys[0] = false;
-				}
-			}
-		}
+		handle_input(&c, &game_running);
 
 		uint16_t opcode = c.memory[c.pc];
 		opcode <<= 8;
@@ -74,4 +57,72 @@ long get_current_time_ms()
     struct timespec spec;
     clock_gettime(CLOCK_REALTIME, &spec);
     return spec.tv_sec * 1000 + round(spec.tv_nsec / 1.0e6);
+}
+
+void handle_input(struct chip8 *c, bool *game_running)
+{
+	SDL_Event e;
+	while(SDL_PollEvent(&e) != 0) {
+		if(e.type == SDL_QUIT) {
+			*game_running = false;
+		} else if(e.type == SDL_KEYDOWN) {
+			update_state(c, e, true);
+		} else if(e.type == SDL_KEYUP) {
+			update_state(c, e, false);
+		}
+	}
+}
+
+void update_state(struct chip8 *c, SDL_Event e, bool value)
+{
+	switch(e.key.keysym.sym) {
+	case SDLK_KP_PERIOD:
+		c->keys[0x0] = value;
+		break;
+	case SDLK_KP_7:
+		c->keys[0x1] = value;
+		break;
+	case SDLK_KP_8:
+		c->keys[0x2] = value;
+		break;
+	case SDLK_KP_9:
+		c->keys[0x3] = value;
+		break;
+	case SDLK_KP_4:
+		c->keys[0x4] = value;
+		break;
+	case SDLK_KP_5:
+		c->keys[0x5] = value;
+		break;
+	case SDLK_KP_6:
+		c->keys[0x6] = value;
+		break;
+	case SDLK_KP_1:
+		c->keys[0x7] = value;
+		break;
+	case SDLK_KP_2:
+		c->keys[0x8] = value;
+		break;
+	case SDLK_KP_3:
+		c->keys[0x9] = value;
+		break;
+	case SDLK_KP_0:
+		c->keys[0xA] = value;
+		break;
+	case SDLK_KP_ENTER:
+		c->keys[0xB] = value;
+		break;
+	case SDLK_KP_DIVIDE:
+		c->keys[0xC] = value;
+		break;
+	case SDLK_KP_MULTIPLY:
+		c->keys[0xD] = value;
+		break;
+	case SDLK_KP_MINUS:
+		c->keys[0xE] = value;
+		break;
+	case SDLK_KP_PLUS:
+		c->keys[0xF] = value;
+		break;
+	}
 }
--- c/src/emulator.h
+++ c/src/emulator.h
@@ -1,7 +1,12 @@
 #ifndef EMULATOR_H
 #define EMULATOR_H
 
+#include "chip8.h"
+#include <SDL2/SDL.h>
+
 void start_program(char *program_path);
 long get_current_time_ms();
+void handle_input(struct chip8 *c, bool *game_running);
+void update_state(struct chip8 *c, SDL_Event key, bool value);
 
 #endif
