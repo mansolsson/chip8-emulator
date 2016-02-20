9004098c565eb090ea8e0081559f97df87fe666a
Måns Olsson <mans2111@gmail.com>
2016-02-20 18:58:13 +0100

    Added sound to emulator and manifest to jar

--- java/src/main/java/mansolsson/chip8/TimerCountdownThread.java
+++ java/src/main/java/mansolsson/chip8/TimerCountdownThread.java
@@ -2,11 +2,14 @@ package mansolsson.chip8;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import javafx.scene.media.AudioClip;
+
 public class TimerCountdownThread extends Thread {
 	private static final long MILLIS_PER_CYCLE = 1000 / 60;
 
 	private Chip8 chip8;
 	private AtomicBoolean running = new AtomicBoolean();
+	private AudioClip sound = new AudioClip(getClass().getClassLoader().getResource("sound/blip.wav").toString());
 
 	public TimerCountdownThread(Chip8 chip8) {
 		this.chip8 = chip8;
@@ -19,8 +22,15 @@ public class TimerCountdownThread extends Thread {
 			long before = System.currentTimeMillis();
 			chip8.getDelayTimer().reduce();
 			chip8.getSoundTimer().reduce();
+			playSound();
 			long after = System.currentTimeMillis();
 			sleepIfNeeded(after - before);
+		}
+	}
+
+	private void playSound() {
+		if (chip8.getSoundTimer().getTime() > 0 && !sound.isPlaying()) {
+			sound.play();
 		}
 	}
 
